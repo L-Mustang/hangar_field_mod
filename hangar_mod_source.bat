86ec544008bbbca8af239c0d605040bdff3b5e17
LordMustang <elionass58@gmail.com>
2020-11-09 16:58:44 +0100

    Initial commit

---
+++ hangar_mod_source.bat
@@ -0,0 +1,25 @@
+@echo off
+
+echo Deleting old source...
+rd /S /Q "hangar_mod_src"
+
+echo Setting up directory tree...
+echo Copying contents...
+
+xcopy /Y /I "content\pkg_local\gameData\environments\weather_types_hangar_field_mod.blk" "hangar_mod_src\content\pkg_local\gameData\environments\"
+xcopy /Y /I "content\pkg_local\gameData\environments\_weather_types.blk" "hangar_mod_src\content\pkg_local\gameData\environments\"
+xcopy /Y /I "content\pkg_local\gameData\environments\_weather_types_hangar_field.blk" "hangar_mod_src\content\pkg_local\gameData\environments\"
+
+xcopy /Y /I "content\pkg_local\gameData\scenes\hangar_field_mission_mod_prem.blk" "hangar_mod_src\content\pkg_local\gameData\scenes\"
+xcopy /Y /I "content\pkg_local\gameData\scenes\hangar_field_mission_mod_noprem.blk" "hangar_mod_src\content\pkg_local\gameData\scenes\"
+
+xcopy /Y /I /S /E "content\pkg_local\gameData\scenes\timePresets" "hangar_mod_src\content\pkg_local\gameData\scenes\timePresets\"
+xcopy /Y /I /S /E "content\pkg_local\gameData\scenes\vehiclePresets" "hangar_mod_src\content\pkg_local\gameData\scenes\vehiclePresets\"
+
+
+xcopy /Y /I "content\pkg_local\levels\hangar_field_mod.blk" "hangar_mod_src\content\pkg_local\levels\"
+xcopy /Y /I "content\pkg_local\levels\hangar_field_mod.bin" "hangar_mod_src\content\pkg_local\levels\"
+xcopy /Y /I "hangar_field_mod.blk" "hangar_mod_src\"
+
+echo Source created
+pause
